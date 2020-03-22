68c4feb63443a3af7bafb027b495400bacaca19f
Stefan <#Stefan2006>
2020-03-22 14:16:50 +0100

    Add Profile--Add icons to assets

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
+import {Component} from 'react';
 import './index.scss';
 import Header from '../../components/Header';
-import Home_icon from '../../Assets/icons/home.svg';
-import Task_icon from '../../Assets/icons/alert-circle.svg';
 import Sidebar from '../../components/Sidebar';
 
 class Template extends Component {
@@ -13,40 +11,63 @@ class Template extends Component {
 
 	render() {
 		return (
-			<div className='dashboard'>
-				<Header side='Dashboard' />
-				<Sidebar></Sidebar>
-				<div className='content'>
-					<div className='box'>
-						<div className='image'>
-							<img src='' alt='fach_logo' />
-						</div>
-						<div className='box-content'>
-							<h3 className='subject_name'></h3>
-							<span className='under_name'></span>
-							<div className='weekend_task'>
-								<img className='Calender_icon' src='' alt='KAlender Icon' />
-								<span className='weekend_task_text'></span>
-							</div>
-							<div className='points'>
-								<div className='user_points'>
-									<img src='' alt='user_icon' className='user_points_image' />
-									<span className='user_text'></span>
-								</div>
-								<div className='community_points'>
-									<img
-										src=''
-										alt='community_icon'
-										className='community_points_image'
-									/>
-									<span className='community_text'></span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
+			< div;
+		className = 'dashboard' >
+			< Header;
+		side = 'Dashboard' / >
+			< Sidebar > < /Sidebar>
+			< div;
+		className = 'content' >
+			< div;
+		className = 'box' >
+			< div;
+		className = 'image' >
+			< img;
+		src = '';
+		alt = 'fach_logo' / >
+			< /div>
+			< div;
+		className = 'box-content' >
+			< h3;
+		className = 'subject_name' > < /h3>
+			< span;
+		className = 'under_name' > < /span>
+			< div;
+		className = 'weekend_task' >
+			< img;
+		className = 'Calender_icon';
+		src = '';
+		alt = 'KAlender Icon' / >
+			< span;
+		className = 'weekend_task_text' > < /span>
+			< /div>
+			< div;
+		className = 'points' >
+			< div;
+		className = 'user_points' >
+			< img;
+		src = '';
+		alt = 'user_icon';
+		className = 'user_points_image' / >
+			< span;
+		className = 'user_text' > < /span>
+			< /div>
+			< div;
+		className = 'community_points' >
+			< img;
+		src = '';
+		alt = 'community_icon';
+		className = 'community_points_image'
+			/ >
+			< span;
+		className = 'community_text' > < /span>
+			< /div>
+			< /div>
+			< /div>
+			< /div>
+			< /div>
+			< /div>;
+	)
 	}
 }
 
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import {Component} from 'react';
 import './index.scss';
-import { Route, Redirect } from 'react-router-dom';
+import {Redirect, Route} from 'react-router-dom';
 import Login from '../../components/Login';
 import Register from '../../components/Register';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import Landing_Image from '../../Assets/Picture/landingpage-teaching.svg';
+import Landing_Image from '../../assets/Picture/landingpage-teaching.svg';
 import Modal from 'react-animated-modal';
 
 class Landing extends Component {
@@ -16,56 +16,97 @@ class Landing extends Component {
 
 	render() {
 		return (
-			<div className='landing'>
-				<Header side='Landing'></Header>
-				<div className='Main'>
-					{(() => {
-						if (this.state.redirect) {
-							this.setState({ redirect: false });
+			< div;
+		className = 'landing' >
+			< Header;
+		side = 'Landing' > < /Header>
+			< div;
+		className = 'Main' >
+			{(() => {
+				if (this.state.redirect) {
+					this.setState({redirect: false});
 
-							return <Redirect exact to={this.state.redirect} />;
-						}
-					})()}
-					<Route
-						exact
-						path={['/login', '/register']}
-						render={() => {
-							if (!this.state.showModal && !this.state.redirect) {
-								this.setState({ showModal: true });
-							}
-						}}
-					></Route>
-					<Modal
-						visible={this.state.showModal}
-						closemodal={() => {
-							console.log('close modal');
-							this.setState({ showModal: false, redirect: '/' });
-						}}
-						type='fadeIn'
-					>
-						<Route exact path='/login' component={Login} />
-						<Route exact path='/register' component={Register} />
-					</Modal>
-					<div id='content'>
-						<div id='image-landigpage'>
-							<img
-								id='landigpage-img'
-								src={Landing_Image}
-								alt='landigpage-teaching image'
-							/>
-						</div>
-						<div id='text-content'>
-							<h2>Education for all</h2>
-							<h4>
-								Keine Ahnung was ich schreiben soll, das ist ein Feature Text. Bitte
-								etwas besseres überlegen
-							</h4>
-						</div>
-					</div>
-				</div>
-				<Footer></Footer>
-			</div>
-		);
+					return;
+				<
+					Redirect;
+					exact;
+					to = {this.state.redirect};
+					/>;
+				}
+			})()
+	}
+	<
+		Route;
+		exact;
+		path = {['/login', '/register';
+	]
+	}
+		render = {();
+	=>
+		{
+			if (!this.state.showModal && !this.state.redirect) {
+				this.setState({showModal: true});
+			}
+		}
+	}
+	><
+		/Route>
+		< Modal;
+		visible = {this.state.showModal};
+		closemodal = {();
+	=>
+		{
+			console.log('close modal');
+			this.setState({showModal: false, redirect: '/'});
+		}
+	}
+		type = 'fadeIn'
+			>
+			< Route;
+		exact;
+		path = '/login';
+		component = {Login};
+		/>
+		< Route;
+		exact;
+		path = '/register';
+		component = {Register};
+		/>
+		< /Modal>
+		< div;
+		id = 'content' >
+			< div;
+		id = 'image-landigpage' >
+			< img;
+		id = 'landigpage-img';
+		src = {Landing_Image};
+		alt = 'landigpage-teaching image'
+			/ >
+			< /div>
+			< div;
+		id = 'text-content' >
+			< h2 > Education;
+		for all < /h2>
+			< h4 >
+			Keine Ahnung;
+		was;
+		ich;
+		schreiben;
+		soll, das;
+		ist;
+		ein;
+		Feature;
+		Text.Bitte;
+		etwas;
+		besseres;
+		überlegen
+		< /h4>
+		< /div>
+		< /div>
+		< /div>
+		< Footer > < /Footer>
+		< /div>;
+	)
 	}
 }
 
